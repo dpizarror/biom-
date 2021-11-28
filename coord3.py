d27e16bdc23aad52a9fb61363c019442696f7136
dpizarror <daniel.pizarro@mail.escuelaing.edu.co>
2021-11-27 19:42:03 -0500

    Update coord3.py

--- coord3.py
+++ coord3.py
@@ -2,6 +2,7 @@
 import btk
 from btkTools import *
 import numpy as np
+from gait_functions import *
 
 filename='JuanDR_Walk01.c3d'
 
@@ -9,3 +10,20 @@ acq=smartReader(filename)
 point_unit=acq.GetPointUnit()
 point_frec=acq.GetPointFrequency()
 marker_names=getMarkerNames(acq)
+
+lmed=acq.GetPoint('LMED').GetValues()
+rmed=acq.GetPoint('RMED').GetValues()
+lasi=acq.GetPoint('LASI').GetValues()
+rasi=acq.GetPoint('RASI').GetValues()
+
+
+LLL = get_leglen(lmed, lasi)
+LLR = get_leglen(rmed, rasi)
+print(f'Largo de piernas. Izquierda: {LLL:.2f} mm, derecha {LLR:.2f} mm')
+
+# hip joint centre
+HJCleft = get_hjc(LLL) 
+HJCright = get_hjc(LLR) 
+#print(f'Coordenadas SACR: {sacr[4,:]}')
+print(f'Coordenadas HJC izquierdo: {HJCleft}')
+print(f'Coordenadas HJC derecho: {HJCright}')
